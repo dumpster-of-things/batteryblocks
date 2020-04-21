66fbefc30d2bddadea148bc428d8873187ab1734
dumpster-of-things <dumpster.of.things@gmail.com>
2020-04-21 17:59:04 -0400

    Improved .gif creation script.

--- Exhibition.sh
+++ Exhibition.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir -p Exhibit
-
 _Discharging() {
 	x=0
 	for n in {100..0}; do
 		echo "0 ${n}" > ~/.config/i3blocks/scripts/Battery.txt
 		i3 restart
 		Num="$(printf '%03d' ${x})"
-		sleep 2
+		sleep 1
 		scrot in_${Num}.png -e "convert in_${Num}.png -trim +repage -gravity northeast -crop 64X20+0+0 Exhibit/0_discharging_${Num}.0.png"
 		if [[ ${n} == 0 ]]; then
 			scrot in_${Num}.png -e "convert in_${Num}.png -trim +repage -gravity northeast -crop 64X20+0+0 Exhibit/0_discharging_${Num}.1.png"
@@ -36,7 +34,7 @@ _Charging() {
 		echo "1 ${n}" > ~/.config/i3blocks/scripts/Battery.txt
 		i3 restart
 		Num="$(printf '%03d' ${n})"
-		sleep 2
+		sleep 1
 		scrot in_${Num}.png -e "convert in_${Num}.png -trim +repage -gravity northeast -crop 64X20+0+0 Exhibit/1_charging_${Num}.0.png"
 		if [[ ${n} == 0 ]]; then
 			scrot in_${Num}.png -e "convert in_${Num}.png -trim +repage -gravity northeast -crop 64X20+0+0 Exhibit/1_charging_${Num}.1.png"
@@ -66,5 +64,31 @@ _Create_GIF() {
 	convert -delay 0 -loop 0 -trim xc:black -gravity center -composite -matte .Converted/*.png exhibit.gif 2>/dev/null
 	rm -rf Exhibit .Converted
 }
+_Ask() {
+	clear
+	echo ' * Make sure you have uncommented the .gif creation option in the main script before proceeding...'
+	echo ""
+	echo 'So...'
+	echo '   the "easiest" way I have found'
+	echo '     - (without having devoted any time to an actually slick/creative solution)'
+	echo '   to create the animated .gif file is to have a script automate the following steps:'
+	echo '1) simulate each charge level for each adapter state...'
+	echo '2) refresh i3...'
+	echo '3) take screentshot using scrot - autocropping to the top-right corner on the fly...'
+	echo '4) sleep 1, then repeat...'
+	echo '5) using ImageMagick; create "exhibit.gif" from the resulting *.png screenshot files'
+	echo '6) cleanup the leftover kruft...'
+	echo ""
+	echo 'The point of this message is to warn you that for the next 200 seconds i3 will reload every 1 second, which might have alarmed you absent this message.'
+	echo 'Let it run, while keeping an eye on the top-right corner of the screen.'
+	echo 'If your battery indicator is not showing up, or you notice something that needs to be changed, simply ctrl+c to stop the script.'
+	echo ' * if you ctrl+c your way out you will need to manually remove the kruft (./Exhibit ./.Converted and ./*.png)'
+	echo ""
+	read -p "Proceed? [Y/n] " Resp
+	if [[ ${Resp} == [yY] ]]; then
+		mkdir -p Exhibit
+		_Discharging && _Charging && _Create_GIF
+	fi
+}
 
-_Discharging && _Charging && _Create_GIF
+_Ask
